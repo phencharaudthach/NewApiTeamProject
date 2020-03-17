4a48b926bab3aff02260477066f87dfe27cd04e9
Snyeswa <53065926+Snyeswa@users.noreply.github.com>
2020-03-16 21:21:07 -0400

    Updates Codes

--- models/user.js
+++ models/user.js
@@ -1,69 +1,35 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
-const crypto = require('crypto')
-const jwt = require('jsonwebtoken')
-const secret = require('../config').secret
+const bcrypt = require('bcrypt-nodejs')
 
+//User schema
 const userSchema = new mongoose.Schema ({
-    username: {
+    name: {
         type: String,
         lowercase: true, 
         unique: true,
         required: [true, "can't be blank"], 
-        match: [/^[a-zA-Z0-9]+$/, 'is invalid'], 
-        index: true
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid']
     },
     email: {
         type: String,
         lowercase: true, 
         unique: true,
         required: [true, "can't be blank"], 
-        match: [/\S+@\S+\.\S+/, 'is invalid'], 
-        index: true
+        match: [/\S+@\S+\.\S+/, 'is invalid']
     },
-    image: String,
-    hash: String,
-    salt: String
+    password: String,
+});
 
-}, {timestamps: true})
-
-userSchema.plugin(uniqueValidator, {message: 'is already taken.'})
-
-//Method to set user passwords
-userSchema.methods.setPassword = function(password){
-    this.salt = crypto.randomBytes(16).toString('hex')
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
-}
-
-//Method to validate passwords
-userSchema.methods.validPassword = function(password){
-    var hash = crpyto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
-    return this.hash === hash
-}
-
-//Method to generate a JSON web token
-userSchema.methods.generateJWT = function () {
-    var today = new Date();
-    var exp = new Date(today);
-    exp.setDate(today.getDate() + 60);
-
-    return jwt.sign({
-        id: this._id,
-        username: this.username,
-        exp: parseInt(exp.getTime() /1000),
-    }, secret);
+//Get password and hash it
+userSchema.methods.encryptPassword = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null)
 };
 
-//Method to get JSON of a user for authentication
-userSchema.methods.toAuthJSON = function(){
-    return{
-        username: this.username,
-        email: this.email,
-        token: this.generateJWT(),
-        image: this.image
-
-    }
-}
+//Check if password matches hashed password
+userSchema.methods.validPassword = (password) => {
+    return bcrypt.compareSync(password, this.password)
+};
 
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express')
+const passport = require('passport')
 const app = express();
 
+//MiddleWare
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//set key for encrypting cookies, and avoid saving unmodified content
+app.use(session({secret: 'secretKey', saveUninitialized: false}))
+
+//Middleware to change user value in request to deserialized user
+app.use(passport.session())
+
+//middleware to initialize passport for authentication
+app.use(passport.initialize())
+require('./config/passport.js')
+// const indexRouter = require('./routes/index')
+// const userRouter = require('./routes/users')
+
+//Connect to Database
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jaliyah:jaliyah123@cluster0-7lgiw.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true  })
 const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.on('open', () => console.log('Connected to the database'))
 
-
-app.listen(3000)
+//Listening to Server
+app.listen(3000, console.log('Listening to Server'))
